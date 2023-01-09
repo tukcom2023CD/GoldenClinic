4832044e9632f40163e32f43fc43145d7c534cd5
권태우 KwonTaewoo <kwontaewoo0901@gmail.com>
2023-01-09 13:12:26 +0900

    Create iOS project

---
+++ BBGG_iOS/BangBangGokGok/BangBangGokGok/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BangBangGokGok
+//
+//  Created by 권태우 on 2023/01/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
