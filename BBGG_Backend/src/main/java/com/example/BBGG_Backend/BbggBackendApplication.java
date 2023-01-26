43747372f41da8c2acc6988bf05344577d8564b0
gu <82009882+kugu98@users.noreply.github.com>
2023-01-26 10:35:05 +0900

    BBGG_Backend
    Java11/Gradle/Spring2.7.8

---
+++ BBGG_Backend/src/main/java/com/example/BBGG_Backend/BbggBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.BBGG_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbggBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbggBackendApplication.class, args);
+	}
+
+}
---
+++ BBGG_Backend/src/test/java/com/example/BBGG_Backend/BbggBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BBGG_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbggBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
