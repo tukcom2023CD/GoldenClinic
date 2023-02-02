f8ea70278c9b536eb43aa472879b1b02cc6ef7ef
gu <82009882+kugu98@users.noreply.github.com>
2023-02-02 15:46:49 +0900

    Mark 정보값
    id/위도/경도/text 값 저장

--- BBGG_Backend/src/main/java/com/example/BBGG_Backend/gps/repository/entity/Mark.java
+++ BBGG_Backend/src/main/java/com/example/BBGG_Backend/gps/repository/entity/Mark.java
@@ -1,4 +1,40 @@
 package com.example.BBGG_Backend.gps.repository.entity;
 
-public class Mark {
+import com.example.BBGG_Backend.gps.repository.dto.Markdto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Validated
+@Builder
+@Getter
+@Table
+public class Mark extends BaseTimeEntity{
+    @Id
+    @GeneratedValue
+    private long seq;
+    @Column
+    private String userId;
+    @Column
+    private double latitude;
+    @Column(nullable = false)
+    private double longitude;
+    @Column
+    private String text;
+
+
+    public Markdto markdto(){
+        return Markdto.builder()
+                .latitude(latitude)
+                .longitude(longitude)
+                .text(text)
+                .build();
+    }
 }
