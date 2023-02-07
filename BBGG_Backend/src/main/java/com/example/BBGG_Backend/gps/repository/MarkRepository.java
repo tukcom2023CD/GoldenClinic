a49bebafe53d1b69fe7f3f0d2a296679ced9824a
gu <82009882+kugu98@users.noreply.github.com>
2023-02-07 14:53:54 +0900

    로그인한 사용자 기록 불러오기

--- BBGG_Backend/src/main/java/com/example/BBGG_Backend/gps/repository/MarkRepository.java
+++ BBGG_Backend/src/main/java/com/example/BBGG_Backend/gps/repository/MarkRepository.java
@@ -3,8 +3,9 @@ package com.example.BBGG_Backend.gps.repository;
 import com.example.BBGG_Backend.gps.repository.entity.Mark;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MarkRepository extends JpaRepository<Mark,Long> {
-    Optional<Mark> findByUserId(String userId);
+    List<Mark> findByUserId(String userId);
 }
