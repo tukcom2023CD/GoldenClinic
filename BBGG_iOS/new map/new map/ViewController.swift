6aac93d50dbc2fe0102767e329eac28ea805e458
권태우 KwonTaewoo <kwontaewoo0901@gmail.com>
2023-03-21 18:58:13 +0900

    Update ViewController.swift

--- BBGG_iOS/new map/new map/ViewController.swift
+++ BBGG_iOS/new map/new map/ViewController.swift
@@ -6,16 +6,23 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    @IBOutlet weak var myMap: MKMapView!
+    
+    let loctionManger = CLLocationManager()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        
+        loctionManger.delegate = self
+        loctionManger.desiredAccuracy = kCLLocationAccuracyBest
+        loctionManger.requestWhenInUseAuthorization()
+        loctionManger.startUpdatingLocation()
+        myMap.showsUserLocation = true
     } // ViewController
 
-
+    
 }
 
