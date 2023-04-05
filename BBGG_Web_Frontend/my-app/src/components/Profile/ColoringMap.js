30b9a616efe7c6830699e4b1748fd85b79850656
typeYu <105202875+typeYu@users.noreply.github.com>
2023-04-06 04:25:06 +0900

    success coloring

--- BBGG_Web_Frontend/my-app/src/components/Profile/ColoringMap.js
+++ BBGG_Web_Frontend/my-app/src/components/Profile/ColoringMap.js
@@ -22,55 +22,74 @@ const ProfileForm = () => {
 
         navigator.geolocation.getCurrentPosition(function () {
 
-            const getData = () => {
-                axios.get("http://localhost:8080/api/vworld/req/data?parameter1=%EC%84%9C%EC%9A%B8%ED%8A%B9%EB%B3%84%EC%8B%9C%20%EC%84%9C%EC%B4%88%EA%B5%AC%20%EC%84%9C%EC%B4%88%EB%8F%99", {
-                    params: {
-                        dong: "서울특별시 서초구 서초동"
-                    }
-                }).then((vword) => {
-                    const coordinates = vword.data.response.result.featureCollection.features[0].geometry.coordinates[0][0];
-                    const points = [];
-                    for (let i = 0; i < coordinates.length; i++) {
-                        const lat = coordinates[i][1];
-                        const lng = coordinates[i][0];
-                        points.push(new kakao.maps.LatLng(lat, lng));
-                    }
-                    const polygon = new kakao.maps.Polygon({
-                        map: map,
-                        path: points,
-                        strokeWeight: 2,
-                        strokeColor: "#004c80",
-                        strokeOpacity: 0.8,
-                        fillColor: "#fff8e1",
-                        fillOpacity: 0.7
-                    });
-                    // 지도에 다각형을 표시합니다
-                    polygon.setMap(map);
-                })
+            function displayMarker(locPosition) {
+
+                var marker = new kakao.maps.Marker({
+                    map: map,
+                    position: locPosition
+                });
+
+                var markers = [];
+
+                markers.push(marker);
+
+                map.setCenter(locPosition);
             };
-            getData();
+
+            //지역들 이름 추출 후 저장
+            const getData = () => {
+
+                axios.get("http://localhost:8080/gps/mark", {
+                    params: {
+                        userId: localStorage.getItem('userId')
+                    }
+                }).then((response) => {
+                    const gpsJson = JSON.stringify(response.data)
+                    const parsedGps = JSON.parse(gpsJson);
+
+                    for (var i = 0; i < parsedGps.length; i++) {
+                        let lat = parsedGps[i].latitude,
+                            lon = parsedGps[i].longitude,
+                            text = parsedGps[i].text;
+
+                        let locPosition = new kakao.maps.LatLng(lat, lon);
+                        console.log(text);
+                        getColoring(text);
+                        displayMarker(locPosition, parsedGps[i].text);
+
+                    }
+                })
+            };getData()
+
+            const getColoring = (text) => {
+                const encodedText = encodeURIComponent(text);
+              
+                axios.get(`http://localhost:8080/api/vworld/req/data?parameter1=${encodedText}`, {
+
+                }).then((vword) => {
+                  const coordinates = vword.data.response.result.featureCollection.features[0].geometry.coordinates[0][0];
+                  const points = [];
+                  for (let i = 0; i < coordinates.length; i++) {
+                    const lat = coordinates[i][1];
+                    const lng = coordinates[i][0];
+                    points.push(new kakao.maps.LatLng(lat, lng));
+                  }
+                  const polygon = new kakao.maps.Polygon({
+                    map: map,
+                    path: points,
+                    strokeWeight: 2,
+                    strokeColor: "#004c80",
+                    strokeOpacity: 0.8,
+                    fillColor: "#fff8e1",
+                    fillOpacity: 0.7
+                  });
+                  // 지도에 다각형을 표시합니다
+                  polygon.setMap(map);
+                });
+              };
+
+
         });
-
-        // // 다각형을 구성하는 좌표 배열입니다. 이 좌표들을 이어서 다각형을 표시합니다
-        // var polygonPath = [
-        //   new kakao.maps.LatLng(37.32912247557823, 126.67091174887754),
-        //   new kakao.maps.LatLng(37.30350064095492, 126.82478476872446),
-        //   new kakao.maps.LatLng(37.40152337758723, 126.8187742026357),
-        // ];
-
-        // // 지도에 표시할 다각형을 생성합니다
-        // var polygon = new kakao.maps.Polygon({
-        //   path: polygonPath, // 그려질 다각형의 좌표 배열입니다
-        //   strokeWeight: 3, // 선의 두께입니다
-        //   strokeColor: '#39DE2A', // 선의 색깔입니다
-        //   strokeOpacity: 0.8, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
-        //   strokeStyle: 'longdash', // 선의 스타일입니다
-        //   fillColor: '#A2FF99', // 채우기 색깔입니다
-        //   fillOpacity: 0.7 // 채우기 불투명도 입니다
-        // });
-
-        // // 지도에 다각형을 표시합니다
-        // polygon.setMap(map);
 
     });
 
