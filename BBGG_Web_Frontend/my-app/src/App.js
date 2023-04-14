934be9dfa158c7fa5d582891c2b4ea0efe6b22b0
typeYu <105202875+typeYu@users.noreply.github.com>
2023-04-14 17:54:02 +0900

    name Area_first >> AreaFirst

--- BBGG_Web_Frontend/my-app/src/App.js
+++ BBGG_Web_Frontend/my-app/src/App.js
@@ -8,7 +8,7 @@ import AuthPage from "./pages/AuthPage";
 import HomePage from "./pages/HomePage";
 import CurrentLocation from "./components/Map/CurrentLocation";
 import TestingPage from "./components/Profile/TestingPage";
-import Area_first from "./components/Record/Area_first";
+import AreaFirst from "./components/Record/AreaFirst";
 import RecordPost from "./components/Record/RecordPost";
 
 function App() {
@@ -21,7 +21,7 @@ function App() {
         <Route path="/CurrentLocation" element={<CurrentLocation />}></Route>
         <Route path="/Cluster" element={<ClusterPage />}></Route>
         <Route path="/Testing" element={<TestingPage />}></Route>
-        <Route path="/Area_first" element={<Area_first />}></Route>
+        <Route path="/AreaFirst" element={<AreaFirst />}></Route>
         <Route path="/RecordPost" element={<RecordPost />}></Route>
       </Routes>
     </Layout>
