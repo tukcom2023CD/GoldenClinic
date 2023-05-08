5ff79841e9312e8f394bc49a2573ce53ee20d384
gu <82009882+kugu98@users.noreply.github.com>
2023-05-08 12:51:07 +0900

    DB 설계 및 데이터 관리

--- BBGG_Backend/src/main/java/com/example/BBGG_Backend/recommend/service/RCService.java
+++ BBGG_Backend/src/main/java/com/example/BBGG_Backend/recommend/service/RCService.java
@@ -1,4 +1,23 @@
 package com.example.BBGG_Backend.recommend.service;
 
+import com.example.BBGG_Backend.recommend.PlaceRepository;
+import com.example.BBGG_Backend.recommend.repository.dto.PlaceDto;
+import com.example.BBGG_Backend.recommend.repository.entity.Place;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class RCService {
+
+    @Autowired
+    private final PlaceRepository placeRepository;
+
+    public String ps(PlaceDto request){
+        placeRepository.save(Place.builder().userId(request.getUserId()).placeName(request.getPlaceName()).address(request.getAddress()).build());
+        return "Success";
+    }
 }
