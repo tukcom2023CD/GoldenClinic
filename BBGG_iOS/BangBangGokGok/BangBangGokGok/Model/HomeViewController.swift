0186ae2be23edf4e65b59decee5b34d4274cce44
권태우 KwonTaewoo <kwontaewoo0901@gmail.com>
2023-05-10 14:27:55 +0900

    Update HomeViewController.swift

--- BBGG_iOS/BangBangGokGok/BangBangGokGok/Model/HomeViewController.swift
+++ BBGG_iOS/BangBangGokGok/BangBangGokGok/Model/HomeViewController.swift
@@ -8,11 +8,19 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    @IBOutlet weak var lblRecoReg: UILabel!
+    @IBOutlet weak var imgBanner: UIImageView!
+    @IBOutlet weak var lblPercentage: UILabel!
+    @IBOutlet weak var progressView: UIProgressView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        lblRecoReg.text = "이달의 추천 여행지"
+        lblPercentage.text = "전국일주 완료율"
+//        progressView.progress 
     }
     
 
@@ -25,5 +33,9 @@ class HomeViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    
+    @IBAction func btnVisited(_ sender: UIButton) { // Show visited regions
+        
+    }
+    
 }
