ece28ce0e0f59fb0cb3e94b7f5de68dc4dbb15c2
typeYu <105202875+typeYu@users.noreply.github.com>
2023-05-11 04:13:06 +0900

    mouse wheel event

--- BBGG_Web_Frontend/my-app/src/components/StartingPage/StartingPageContent.js
+++ BBGG_Web_Frontend/my-app/src/components/StartingPage/StartingPageContent.js
@@ -3,6 +3,21 @@ import { useState, useEffect } from "react";
 
 const StartingPageContent = () => {
   const [isLogin, setIsLogin] = useState(false);
+  const [page, setPage] = useState(1);
+
+  const posTop = (page - 1) * window.innerHeight;
+    window.scrollTo({ top: posTop, behavior: 'smooth' });
+
+  const handleScroll = (e) => {
+    if (e.deltaY > 0) {
+      if (page === 3) return;
+      setPage(page + 1);
+    } else if (e.deltaY < 0) {
+      if (page === 1) return;
+      setPage(page - 1);
+    }
+    
+  };
 
   useEffect(() => {
     if (localStorage.getItem('userId') === null) {
@@ -18,15 +33,19 @@ const StartingPageContent = () => {
     }
   }
 
+  window.addEventListener("wheel", function (e) {
+    e.preventDefault();
+  }, { passive: false });
+
 
   return (
-    <div><div className={classes.headerSpacer}>.</div>
-    <button className={classes.yellow}>앱 다운로드 ⤓</button>
+    <div onWheel={handleScroll}><div className={classes.headerSpacer}>.</div>
+      <button className={classes.yellow}>앱 다운로드 ⤓</button>
       <div className={classes.starting2}>
         <h1>TO TRAVEL IS TO LIVE</h1>
       </div>
       <div className={classes.starting3}>
-        <h1>한번쯤<br/>꿈에 그리던<br/>전국일주</h1>
+        <h1>한번쯤<br />꿈에 그리던<br />전국일주</h1>
       </div>
       <div className={classes.starting}>
         <h1>웹사이트에서 시작하기</h1>
