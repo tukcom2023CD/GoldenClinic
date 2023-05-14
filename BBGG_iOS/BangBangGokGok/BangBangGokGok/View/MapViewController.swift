f020aa51686701fb31e9ee2f3dfa4ce350bbd011
권태우 KwonTaewoo <kwontaewoo0901@gmail.com>
2023-05-14 12:46:55 +0900

    Update MapViewController.swift

--- BBGG_iOS/BangBangGokGok/BangBangGokGok/View/MapViewController.swift
+++ BBGG_iOS/BangBangGokGok/BangBangGokGok/View/MapViewController.swift
@@ -32,7 +32,16 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
         setAnnotation(latitudeValue: 36.15299, longitudeValue: 128.34538, delta: 1, title: "구미시", subtitle: "경상북도 구미시")
         setAnnotation(latitudeValue: 37.33972, longitudeValue: 126.73354, delta: 1, title: "시흥시", subtitle: "경기도 시흥시")
         setAnnotation(latitudeValue: 35.89086, longitudeValue: 128.59930, delta: 1, title: "대구광역시", subtitle: "대구광역시")
-        
+        setAnnotation(latitudeValue: 35.1798, longitudeValue: 129.0756, delta: 1, title: "부산광역시", subtitle: "부산광역시")
+        setAnnotation(latitudeValue: 35.1601, longitudeValue: 126.8514, delta: 1, title: "광주광역시", subtitle: "광주광역시")
+        setAnnotation(latitudeValue: 36.3552, longitudeValue: 128.7049, delta: 1, title: "의성군", subtitle: "경상북도 의성군")
+        setAnnotation(latitudeValue: 34.8119, longitudeValue: 126.3928, delta: 1, title: "목포시", subtitle: "전라남도 목포시")
+        setAnnotation(latitudeValue: 37.3202, longitudeValue: 126.8309, delta: 1, title: "안산시", subtitle: "경기도 안산시")
+        setAnnotation(latitudeValue: 38.1473, longitudeValue: 127.3047, delta: 1, title: "철원군", subtitle: "강원도 철원군")
+        setAnnotation(latitudeValue: 33.5008, longitudeValue: 126.5467, delta: 1, title: "제주시", subtitle: "제주특별자치도 제주시")
+        setAnnotation(latitudeValue: 33.2481, longitudeValue: 126.5622, delta: 1, title: "서귀포시", subtitle: "제주특별자치도 서귀포시")
+        setAnnotation(latitudeValue: 36.4801, longitudeValue: 127.2892, delta: 1, title: "세종특별자치시", subtitle: "세종특별자치시")
+
         let center = CLLocationCoordinate2D(latitude: 36.5, longitude: 127.5)
         let span = MKCoordinateSpan(latitudeDelta: 5, longitudeDelta: 5)
         let region = MKCoordinateRegion(center: center, span: span)
