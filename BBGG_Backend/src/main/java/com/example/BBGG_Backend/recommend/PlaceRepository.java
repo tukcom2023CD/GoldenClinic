89c52f8017aeed5b460c82665510b9b2d95ccde3
gu <82009882+kugu98@users.noreply.github.com>
2023-05-21 15:18:46 +0900

    여행 계획 장소 List

--- BBGG_Backend/src/main/java/com/example/BBGG_Backend/recommend/PlaceRepository.java
+++ BBGG_Backend/src/main/java/com/example/BBGG_Backend/recommend/PlaceRepository.java
@@ -1,5 +1,6 @@
 package com.example.BBGG_Backend.recommend;
 
+import com.example.BBGG_Backend.gps.repository.entity.Mark;
 import com.example.BBGG_Backend.login.repository.entity.User;
 import com.example.BBGG_Backend.recommend.repository.entity.Place;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,5 +11,5 @@ import java.util.Optional;
 @Repository
 public interface PlaceRepository extends JpaRepository<Place, Long> {
     List<Place> findByUserIdAndAddress(String userId, String address);
-
+    List<Place> findByUserId(String userId);
 }
