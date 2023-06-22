2b0f6861a724f5085424f9bb20fd01e66ce7750a
권태우 KwonTaewoo <kwontaewoo0901@gmail.com>
2023-06-22 15:25:43 +0900

    Add many things

---
+++ BBGG_iOS/BangBangGokGok/BangBangGokGok/Model/DetailImage.swift
@@ -0,0 +1,13 @@
+//
+//  DetailImage.swift
+//  BangBangGokGok
+//
+//  Created by 권태우 on 2023/06/22.
+//
+
+import Foundation
+
+class DetailImage {
+    // Properties
+    static var imgName: String = ""
+}
--- BBGG_iOS/BangBangGokGok/BangBangGokGok/View/TripPlanningViewController.swift
+++ BBGG_iOS/BangBangGokGok/BangBangGokGok/View/TripPlanningViewController.swift
@@ -13,6 +13,7 @@ class TripPlanningViewController: UIViewController, CLLocationManagerDelegate, M
     @IBOutlet weak var tripMapView: MKMapView!
     
     var dataArray: [String] = []
+    let loctionManger = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +23,30 @@ class TripPlanningViewController: UIViewController, CLLocationManagerDelegate, M
         
         planningTableView.dataSource = self
         planningTableView.delegate = self
+        
+        tripMapView.delegate = self
+        loctionManger.delegate = self
+        loctionManger.desiredAccuracy = kCLLocationAccuracyBest
+        loctionManger.requestWhenInUseAuthorization()
+        loctionManger.startUpdatingLocation()
+        
+        tripMapView.showsUserLocation = true // show user location
+        tripMapView.isRotateEnabled = false // rotation available
+        tripMapView.isPitchEnabled = false // angle change available
+        
+        setAnnotation(latitudeValue: 37.56471, longitudeValue: 126.97512, delta: 1, title: "서울특별시", subtitle: "서울특별시")
+        setAnnotation(latitudeValue: 36.15299, longitudeValue: 128.34538, delta: 1, title: "구미시", subtitle: "경상북도 구미시")
+        setAnnotation(latitudeValue: 37.33972, longitudeValue: 126.73354, delta: 1, title: "시흥시", subtitle: "경기도 시흥시")
+        setAnnotation(latitudeValue: 35.89086, longitudeValue: 128.59930, delta: 1, title: "대구광역시", subtitle: "대구광역시")
+        setAnnotation(latitudeValue: 35.1798, longitudeValue: 129.0756, delta: 1, title: "부산광역시", subtitle: "부산광역시")
+        setAnnotation(latitudeValue: 35.1601, longitudeValue: 126.8514, delta: 1, title: "광주광역시", subtitle: "광주광역시")
+        setAnnotation(latitudeValue: 36.3552, longitudeValue: 128.7049, delta: 1, title: "의성군", subtitle: "경상북도 의성군")
+        setAnnotation(latitudeValue: 34.8119, longitudeValue: 126.3928, delta: 1, title: "목포시", subtitle: "전라남도 목포시")
+
+//        let center = CLLocationCoordinate2D(latitude: 36.5, longitude: 127.5)
+//        let span = MKCoordinateSpan(latitudeDelta: 5, longitudeDelta: 5)
+//        let region = MKCoordinateRegion(center: center, span: span)
+//        tripMapView.setRegion(region, animated: true)
     }
     
     func dataInit() {
@@ -68,4 +93,45 @@ class TripPlanningViewController: UIViewController, CLLocationManagerDelegate, M
         return cell
     }
 
+    func goLocation(latitudeValue: CLLocationDegrees, longitudeValue: CLLocationDegrees, delta span: Double) -> CLLocationCoordinate2D {
+        let pLocation = CLLocationCoordinate2DMake(latitudeValue, longitudeValue)
+        let spanValue = MKCoordinateSpan(latitudeDelta: span, longitudeDelta: span)
+        let pRegion = MKCoordinateRegion(center: pLocation, span: spanValue)
+        
+        tripMapView.setRegion(pRegion, animated: true)
+        
+        return pLocation
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let pLocation = locations.last
+        _ = goLocation(latitudeValue: (pLocation?.coordinate.latitude)!, longitudeValue: (pLocation?.coordinate.longitude)!, delta: 0.01)
+        
+        CLGeocoder().reverseGeocodeLocation(pLocation!, completionHandler: {
+            (placemarks, error) -> Void in
+            let pm = placemarks!.first
+            let country = pm!.country
+            var adress:String = country!
+            if pm!.locality != nil {
+                adress += " "
+                adress += pm!.locality!
+            }
+            if pm!.thoroughfare != nil {
+                adress += " "
+                adress += pm!.thoroughfare!
+            }
+            
+        })
+        
+    }
+
+    func setAnnotation(latitudeValue: CLLocationDegrees, longitudeValue: CLLocationDegrees, delta span: Double, title strTitle: String, subtitle strSubtitle: String){
+        let annotaion  = MKPointAnnotation()
+        
+        annotaion.coordinate = goLocation(latitudeValue: latitudeValue, longitudeValue: longitudeValue, delta: span)
+        annotaion.title = strTitle
+        annotaion.subtitle = strSubtitle
+        
+        tripMapView.addAnnotation(annotaion)
+    }
 }
