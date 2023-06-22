b45a7d881d07667497401e6b364896cfdb819c98
권태우 KwonTaewoo <kwontaewoo0901@gmail.com>
2023-06-22 15:43:22 +0900

    Update TripPlanningViewController.swift

--- BBGG_iOS/BangBangGokGok/BangBangGokGok/View/TripPlanningViewController.swift
+++ BBGG_iOS/BangBangGokGok/BangBangGokGok/View/TripPlanningViewController.swift
@@ -34,19 +34,19 @@ class TripPlanningViewController: UIViewController, CLLocationManagerDelegate, M
         tripMapView.isRotateEnabled = false // rotation available
         tripMapView.isPitchEnabled = false // angle change available
         
-        setAnnotation(latitudeValue: 37.56471, longitudeValue: 126.97512, delta: 1, title: "서울특별시", subtitle: "서울특별시")
-        setAnnotation(latitudeValue: 36.15299, longitudeValue: 128.34538, delta: 1, title: "구미시", subtitle: "경상북도 구미시")
-        setAnnotation(latitudeValue: 37.33972, longitudeValue: 126.73354, delta: 1, title: "시흥시", subtitle: "경기도 시흥시")
-        setAnnotation(latitudeValue: 35.89086, longitudeValue: 128.59930, delta: 1, title: "대구광역시", subtitle: "대구광역시")
-        setAnnotation(latitudeValue: 35.1798, longitudeValue: 129.0756, delta: 1, title: "부산광역시", subtitle: "부산광역시")
-        setAnnotation(latitudeValue: 35.1601, longitudeValue: 126.8514, delta: 1, title: "광주광역시", subtitle: "광주광역시")
-        setAnnotation(latitudeValue: 36.3552, longitudeValue: 128.7049, delta: 1, title: "의성군", subtitle: "경상북도 의성군")
-        setAnnotation(latitudeValue: 34.8119, longitudeValue: 126.3928, delta: 1, title: "목포시", subtitle: "전라남도 목포시")
+        setAnnotation(latitudeValue: 37.478194, longitudeValue: 127.010413, delta: 1, title: "예술의 전당", subtitle: "서울특별시")
+        setAnnotation(latitudeValue: 37.577617, longitudeValue: 126.976537, delta: 1, title: "경복궁", subtitle: "서울특별시")
+        setAnnotation(latitudeValue: 37.511450, longitudeValue: 127.098021, delta: 1, title: "롯데월드", subtitle: "서울특별시")
+        setAnnotation(latitudeValue: 37.551169, longitudeValue: 126.988227, delta: 1, title: "N서울타워", subtitle: "서울특별시")
+        setAnnotation(latitudeValue: 37.548225, longitudeValue: 126.918556, delta: 1, title: "옥동식 합정점", subtitle: "서울특별시")
+        setAnnotation(latitudeValue: 37.497214, longitudeValue: 127.033417, delta: 1, title: "을밀대 강남점", subtitle: "서울특별시")
+        setAnnotation(latitudeValue: 37.584107, longitudeValue: 126.974248, delta: 1, title: "청와대", subtitle: "서울특별시")
+        setAnnotation(latitudeValue: 37.519535, longitudeValue: 126.940993, delta: 1, title: "63빌딩", subtitle: "서울특별시")
 
-//        let center = CLLocationCoordinate2D(latitude: 36.5, longitude: 127.5)
-//        let span = MKCoordinateSpan(latitudeDelta: 5, longitudeDelta: 5)
-//        let region = MKCoordinateRegion(center: center, span: span)
-//        tripMapView.setRegion(region, animated: true)
+        let center = CLLocationCoordinate2D(latitude: 37.56471, longitude: 126.97512)
+        let span = MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+        let region = MKCoordinateRegion(center: center, span: span)
+        tripMapView.setRegion(region, animated: true)
     }
     
     func dataInit() {
